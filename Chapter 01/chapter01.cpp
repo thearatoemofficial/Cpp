1e507fb9f491dcf42588fdbfa3c2021f40f381c6
TOEM Theara <thearatoem.official@gmail.com>
2026-06-03 10:25:45 +0700

    update: chapter 01

---
+++ Chapter 01/chapter01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  // Library for input and output
+
+using namespace std;
+
+int main()  // Main function
+{
+    // Print the message to the screen
+    cout << "Welcome to Basic Programming Language" << endl;
+
+    return 0;  // End of program
+}
